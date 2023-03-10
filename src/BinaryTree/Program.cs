fe384319ebd38450fcc7b0a06be568c54d968d40
sergio-echigo <sergio-echigo.wipe764@passmail.net>
2023-03-10 00:21:55 -0300

    Search binary trees

---
+++ src/BinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using BinaryTree;
+
+// Search Binary Trees
+SearchNode binaryTreeAbb = new SearchNode();
+
+binaryTreeAbb.Add(20);
+binaryTreeAbb.Add(50);
+
+binaryTreeAbb.Add(15);
+binaryTreeAbb.Add(10);
+binaryTreeAbb.Add(18);
+binaryTreeAbb.Add(17);
+binaryTreeAbb.Add(16);
+binaryTreeAbb.Add(14);
+binaryTreeAbb.Add(13);
+
+Console.WriteLine("Old: ");
+binaryTreeAbb.Print();
+
+Console.WriteLine();
+binaryTreeAbb.Delete(15);
+
+Console.WriteLine("New: ");
+binaryTreeAbb.Print();
---
+++ src/BinaryTree/SearchNode.cs
@@ -0,0 +1,171 @@
+namespace BinaryTree;
+
+// Every element at the left must to be less than its root's value.
+// Every element at the right must to be grather or equal than its root's value.
+/// Left and right trees are ABB too.
+
+public class SearchNode
+{
+    private double? _value;
+
+    private SearchNode? _left = null;
+    private SearchNode? _right = null;
+
+    private SearchNode? _parent = null;
+
+    public SearchNode() { }
+    private SearchNode(double value, SearchNode parent)
+    {
+        this._value = value;
+        _parent = parent;
+    }
+
+    /// <summary>
+    /// Searches for the right position to the new node to be included and adds it.
+    /// </summary>
+    public void Add(double value)
+    {
+        if (this._value is null)
+        {
+            this._value = value;
+        }
+        else if (value < this._value)
+        {
+            if (this._left is not null)
+                this._left.Add(value);
+            else
+                this._left = new SearchNode(value, this);
+        }
+        else
+        {
+            if (this._right is not null)
+                this._right.Add(value);
+            else
+                this._right = new SearchNode(value, this);
+        }
+    }
+
+    /// <summary>
+    /// Searches for the right node to delete and deletes it.
+    /// </summary>
+    public void Delete(double value)
+    {
+        // If value to delete is less than actual node's value, searches in the left child node and removes it.
+        if (value < this._value)
+        {
+            if (this._left is not null)
+                this._left.Delete(value);
+
+        }
+        // If value to delete is grather than actual node's value, searches in the right child node and removes it.
+        else if (value > this._value)
+        {
+            if (this._right is not null)
+                this._right.Delete(value);
+        }
+        // If actual node's value equals to value to delete, deletes it.
+        else
+        {
+            // In case of actual node being leaf node.
+            if (this.IsLeafNode())
+            {
+                if (this._parent is not null)
+                {
+                    if (value > this._parent._value)
+                    {
+                        this._parent._right = null;
+                    }
+                    else
+                    {
+                        this._parent._left = null;
+                    }
+                }
+                else
+                {
+                    this._value = null;
+                }
+            }
+            else
+            {
+                // If parent node isn't null (we are not the root node)
+                if (this._parent is not null)
+                {
+                    // We're the left child node from parent.
+                    if (this._parent._value > value)
+                    {
+                        if (this._left is null)
+                            this._parent._left = this._right;
+                        else if (this._right is null)
+                            this._parent._left = this._left;
+                        else
+                        {
+                            SearchNode n = this._right;
+                            while(n!._left is not null)
+                                n = n._left;
+
+                            this._parent._left = this._right;
+                            n._left = this._left;
+                        }
+                    }
+                    // We're the right child node from parent.
+                    else
+                    {
+                        if (this._left is null)
+                            this._parent._right = this._right;
+                        else if (this._right is null)
+                            this._parent._right = this._left;
+                        else
+                        {
+                            SearchNode n = this._right;
+                            while(n!._left is not null)
+                                n = n._left;
+
+                            Console.WriteLine("n is " + n);
+                            this._parent._right = this._right;
+
+                            n._left = this._left;
+                        }
+
+                    }
+                }
+                else
+                {
+                    this._value = null;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Prints the whole binary tree. The number inside () represents the level.
+    /// </summary>
+    public void Print(int level = 0)
+    {
+        if (this._left is not null)
+            this._left.Print(level + 1);
+        
+        Console.Write($" {this._value}({level})");
+
+        if (this._right is not null)
+            this._right.Print(level + 1);
+    }
+
+    /// <summary>
+    /// Verifies if the node is a leaf node.
+    /// </summary>
+    public bool IsLeafNode() =>
+        this._left is null && this._right is null;
+
+    /// <sumary>
+    /// Sets all properties to those provided.
+    /// </summary>
+    public void Set(double? value, SearchNode? left, SearchNode? right, SearchNode? parent)
+    {
+        this._value = value;
+
+        this._left = left;
+        this._right = right;
+
+        this._parent = parent;
+    }
+}
